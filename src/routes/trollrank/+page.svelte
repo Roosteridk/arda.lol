4164a70c4b4e706c2cb40f1fdefc7f24dcf89d4d
Arda <roosterdogan@gmail.com>
2023-05-19 09:51:34 -0400

    first commit

---
+++ src/routes/trollrank/+page.svelte
@@ -0,0 +1,34 @@
+<input type="text" bind:value={query} />
+
+<script>
+    import users from "./users.json"
+    let query = ""
+
+    // Only include results that begin with the query, sorted by rank
+    $: results = users.filter(user => user.user.startsWith(query)).sort((a, b) => a.rank - b.rank).map(user => ({...user, rank: user.rank + 1}))
+</script>
+
+<table>
+    <tr>
+        <th>Rank</th>
+        <th>User</th>
+        <th>Score</th>
+    </tr>
+    {#each results.slice(0, 100) as user}
+        <tr>
+            <td>{user.rank}</td>
+            <td><a href="https://eksisozluk1923.com/biri/{user.user}" target="_blank">{user.user}</a></td>
+            <td>{user.score}</td>
+        </tr>
+    {/each}
+</table>
+
+
+<style>
+    input {
+        width: 500px;
+        padding: 10px;
+        font-size: 16px;
+        border: 1px solid #ccc;
+    }
+</style>
